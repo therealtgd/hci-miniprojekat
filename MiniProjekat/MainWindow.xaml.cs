21149c24c2f547bf7d994e284e4e928151f44ad4
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-24 14:09:12 -0700

    Adds toggle switch for graph type

--- MiniProjekat/MainWindow.xaml.cs
+++ MiniProjekat/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace MiniProjekat
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainPage_Loaded;
         }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            tgl1.Checked += Tgl1_Checked;
+            tgl1.Unchecked += Tgl1_Unchecked;
+            tgl2.Checked += Tgl2_Checked;
+            tgl2.Unchecked += Tgl2_Unchecked;
+            tgl1.IsChecked = true;
+        }
+
+        private void Tgl2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            tgl1.IsChecked = true;
+        }
+
+        private void Tgl2_Checked(object sender, RoutedEventArgs e)
+        {
+            tgl1.IsChecked = false;
+        }
+
+        private void Tgl1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            tgl2.IsChecked = true;
+        }
+
+        private void Tgl1_Checked(object sender, RoutedEventArgs e)
+        {
+            tgl2.IsChecked = false;
+        }
+    
     }
 }
